889e5f54d6e82b8a9ab3374ecfa7143bf1c56732
Senturos <josh_mc94@hotmail.com>
2026-09-10 23:55:30 +1000

    Restore CommonLibSSE-NG plugin rule before legacy-runtime rebuild

--- xmake.lua
+++ xmake.lua
@@ -1,16 +1,19 @@
 includes("lib/commonlibsse-ng")
 
 set_project("AzuraStarSound")
-set_version("1.0.1")
+set_version("1.0.0")
 set_languages("c++23")
 set_warnings("allextra")
 
 add_rules("mode.debug", "mode.releasedbg", "mode.release")
 
 target("AzuraStarSound")
-    set_kind("shared")
-    set_arch("x64")
     add_deps("commonlibsse-ng")
+    add_rules("commonlibsse-ng.plugin", {
+        name = "Azura Star Sound",
+        author = "Senturos",
+        description = "Makes the actual vanilla Azura's Star emit a looping positional sound while loaded in the world."
+    })
     add_files("src/**.cpp")
     add_headerfiles("src/**.h")
     add_includedirs("src")
